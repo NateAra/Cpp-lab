431ffeeaccbeff30e3c47902f60b532aaf905d9a
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-22 13:48:09 +0200

    Experimenting with reading from the console

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,11 @@
 using namespace std;
 
 int main() {
-    int totalSales = 95000;
-    const double stateTax = 0.04;
-    const double countyTax = 0.02;
+    int age;
 
-    double stateTaxTotal = stateTax * totalSales;
-    double countyTaxTotal = countyTax * totalSales;
-    double taxTotal = stateTaxTotal + countyTaxTotal;
-    double totalProfit = totalSales - taxTotal;
+    cout << "Enter Age: ";
+    cin >> age;
+    cout << "Your age is: " << age << endl;
 
-    cout << "Total Sales: $" << totalSales << endl;
-    cout << "State Tax: $" << stateTaxTotal << endl;
-    cout << "County Tax: $" << countyTaxTotal << endl;
-    cout << "Total Tax: $" << taxTotal << endl;
-    cout << "Total Profit: $" << totalProfit << endl;
     return 0;
 }
