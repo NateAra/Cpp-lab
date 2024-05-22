6365b561c31da31bba98059b4577bdbef8ccdd27
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-22 13:01:48 +0200

    Exercis: 1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 
-int main() {
-    int x = 10;
-    double y = 5;
-    double z = (x + 10) / (3 * y);
+using namespace std;
 
-    std::cout << z << std::endl;
+/* Exercise:
+    * Sales: 95, 000
+    * State Tax: 4%
+    * County Tax: 2%
+    * Show you total sales
+    * Show how much state tax you need to pay
+    * Show how much county tax you need to pay
+    * Show how much is your Total tax you have to pay
+*/
+
+
+int main() {
+    int totalSales = 95000;
+    const double stateTax = 0.04;
+    const double countyTax = 0.02;
+
+    double stateTaxTotal = stateTax * totalSales;
+    double countyTaxTotal = countyTax * totalSales;
+    double taxTotal = stateTaxTotal + countyTaxTotal;
+    double totalProfit = totalSales - taxTotal;
+
+    cout << "Total Sales: $" << totalSales << endl;
+    cout << "State Tax: $" << stateTaxTotal << endl;
+    cout << "County Tax: $" << countyTaxTotal << endl;
+    cout << "Total Tax: $" << taxTotal << endl;
+    cout << "Total Profit: $" << totalProfit << endl;
     return 0;
 }
+
+
