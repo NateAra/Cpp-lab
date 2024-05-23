a80b514879e9dbae2ffed98ad40e455661a7f474
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-23 12:15:36 +0200

    Experimenting with Formating Output

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
 #include "ctime"
 #include "cstdlib"
-
-/* Exercise
-    * Write a program for rolling dice.
-        * Every time the dice rolls we will get 2 random number 1 - 6.
-        * To limit the random use this formula: [rand() % (maxValue - minValue + 1)] + minValue
- */
+#include "iomanip"
 
 
 int main() {
-    const int minValue = 1;
-    const int maxValue = 7;
-
-    std::srand(time(0));
-    int dice1 = std::rand() % (maxValue - minValue) + minValue;
-    int dice2 = std::rand() % (maxValue - minValue) + minValue;
-
-    std::cout << "Dice 1: " << dice1 << std::endl;
-    std::cout << "Dice 2: " << dice2 << std::endl;
+    std::cout << std::left;
+    std::cout << std::setw(15) << "Spring " << std::endl << std::setw(15) << "Summer" << std::endl;
 
     return 0;
 }
