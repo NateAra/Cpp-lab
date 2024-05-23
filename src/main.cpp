8afef82c57fb6421bf0cfa5b43bb409ff5586de8
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-23 13:59:30 +0200

    Experimenting with Data type size and limits of int

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,11 @@
 
 
 int main() {
-    std::cout << std::numeric_limits<int>::min() << std::endl << std::numeric_limits<int>::max();
+    std::cout << std::numeric_limits<int>::min() << std::endl << std::numeric_limits<int>::max() << std::endl;
+
+    int number = std::numeric_limits<int>::min();
+    number--;
+    std::cout << number;
 
     return 0;
 }
