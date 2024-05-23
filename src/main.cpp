5399068a52f8f56dc1c6bb30c1557e7338491edb
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-23 15:48:37 +0200

    Experimenting with Basic Type conversion

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,12 @@
 #include "cstdlib"
 #include "iomanip"
 
-/* Exercise
-    *  Write a program that asks the user for 3 names
-    *  Store these names in an array
-    *  Than print the first name.
-*/
-
 int main() {
-    std::string names[3];
+    int x = 10;
+    int y = 3;
+    double z = x / static_cast<double >(y);
 
-    std::cout << "Enter 3 Names Please." << std::endl;
-
-    std::cout << "1st Names: ";
-    std::getline(std::cin, names[0]);
-
-    std::cout << "2nd Names: ";
-    std::getline(std::cin, names[1]);
-
-    std::cout << "3rd Names: ";
-    std::getline(std::cin, names[2]);
-
-    std::cout << "The 1st name is: " << names[0];
+    std::cout << z;
 
     return 0;
 }
