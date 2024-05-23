36d72a59b2b287ef98a55f336d4dd063879e803a
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-23 12:26:36 +0200

    Experimenting with Formating Output: fixed and setprecision

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 
 int main() {
+    std::cout << std::fixed << std::setprecision(2) << 12.34567 << std::endl << 12.54 << std::endl;
     std::cout << std::left;
     std::cout << std::setw(15) << "Spring " << std::endl << std::setw(15) << "Summer" << std::endl;
 
