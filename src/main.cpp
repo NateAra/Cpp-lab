1a99763b9f30f3a78b8f6194981964ac24933711
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-23 13:56:38 +0200

    Experimenting with Data type size and limits

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,9 @@
 #include "cstdlib"
 #include "iomanip"
 
-/* Exercise
-    * Write a program for printing this table:
-        * |Course      |Student      |
-        * |C++         |          100|
-        * |JavaScript  |           50|
-*/
-
 
 int main() {
-    const int cppStudents = 100;
-    const int jsStudents = 50;
-
-    std::cout << std::left << "|" << std::setw(15) << "Course" << "|" << std::setw(10) << "Student" << "|" << std::endl;
-    std::cout << std::left << "|" << std::setw(15) << "C++" << "|" << std::right << std::setw(10) << cppStudents << "|" << std::endl;
-    std::cout << std::left << "|" << std::setw(15) << "JavaScript" << "|" << std::right << std::setw(10) << jsStudents << "|" << std::endl;
+    std::cout << std::numeric_limits<int>::min() << std::endl << std::numeric_limits<int>::max();
 
     return 0;
 }
