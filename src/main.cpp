7ae2eca15849c5d3c9b2a1e048852990f072307b
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-25 12:09:10 +0200

    Experimenting with Basic Nested if statement

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,20 @@
 
 
 /* Exercise
-    * Use this chart to set the commission based on the sales
-        * Sale | Commission
-        * Up to 10,000 | 10 %
-        * 10,000 to 50,000 \ 15 %
-        * Over 50,000 | 20 %
+    *
 */
 int main() {
 
-    double lowCommission = 0.10;
-    double middleCommission = 0.15;
-    double highCommission = 0.20;
+    bool isCitizen = true;
+    bool caResident = true;
+    int tuition = 0;
 
-    int lowSales = 10'000;
-    int highSales = 15'000;
-
-    int sales;
-    std::cout << "How much was the sale? ";
-
-    std::cin >> sales;
-
-    if (sales < lowSales) {
-        std::cout << sales * lowCommission << std::endl << "10% Commission";
-    } else if (sales >= lowSales && sales <= highSales) {
-        std::cout << sales * middleCommission << std::endl << "15% Commission";
+    if (isCitizen) {
+        if (!caResident) tuition = 1000;
     } else {
-        std::cout << sales * highCommission << std::endl << "20% Commission";
+        tuition = 3000;
     }
+
 
     return 0;
 }
