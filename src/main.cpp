0951ea9f5c01ecbd2d1f996d37626e2a7641ddfd
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-27 16:25:32 +0200

    Experimenting with Declaring Functions

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,16 @@
 
 using namespace std;
 
-const double taxRate = .2;
-
-double calculateTax(int sales) {
-    return sales * taxRate;
-}
+// Function declaration (Function prototype)
+void greet(string name);
 
 int main() {
 
-    int sales = 10'000;
-    double tax = calculateTax(sales);
-    cout << tax;
 
     return 0;
+}
+
+// Function definition
+void greet(string name) {
+    cout << "Hello " << name;
 }
