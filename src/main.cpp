782243e7840b4db7dbd3bd0fffe6176e44b5c9f6
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-27 13:36:21 +0200

    Experimenting with basic Defining and Calling Functions

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,13 @@
         - ****
 */
 
-int main() {
-    int rows = 0;
-    std::cout << "Enter a rows: ";
-    std::cin >> rows;
+void greet(std::string name) {
+    std::cout << "Hello " << name << "!";
+}
 
-    for (int x = 1; x <= rows ; x++) {
-        for (int y = 0; y < x; y++) {
-            std::cout << "*" ;
-        }
-        std::cout << std::endl;
-    }
+int main() {
+
+    greet("Nate");
 
     return 0;
 }
