aec746bc2152040f5d04aa215a6b0ec17f1751c4
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-28 12:46:24 +0200

    Experimenting with Copying Arrays

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,15 @@
 
 int main() {
 
-    int numbers[] = {10, 20, 30};
+    int first[] = {10, 20, 30};
+    int second[std::size(first)];
 
-    for (int i = 0; i < std::size(numbers) ; ++i) {
-        std::cout << numbers[i] << std::endl;
+    for (int i = 0; i < std::size(first) ; i++) {
+        second[i] = first[i];
+    }
+
+    for (int number : second) {
+        std::cout << number << std::endl;
     }
 
     return 0;
