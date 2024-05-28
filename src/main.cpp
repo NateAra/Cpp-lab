0ee6e7a6cc93c5ec85a52b6e82e848f869ae2faa
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-28 14:19:13 +0200

    Experimenting with Linear Searching Arrays

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include "../utils/greet.hpp"
 
+/* Exercise
+    * Implement the linear search algorithm
+        * Create a function for finding a value in an int array.
+        * If the target value exists, return its index. Otherwise, return -1.
+*/
+
+int findValue(int numbers[], int size, int val) {
+    for (int i = 0; i < size; ++i) {
+        if (numbers[i] == val) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 
 int main() {
 
-    int value[3] = {10, 20, 30};
+    int numbers[] = {1, 3, 5, 11, 8, 12};
+    int value = 2;
 
-    // Structured binding
-    // Or Unpacking Arrays
-    int x = value[0];
-    int y = value[1];
-    int z = value[2];
-
-    auto [a, b, c] = value;
-
-    std::cout << x << ", " << y << ", " << z << std::endl;
-    std::cout << a << ", " << b << ", " << c << std::endl;
+    std::cout << findValue(numbers, std::size(numbers), value);
 
 
     return 0;
