82285c010134601352e3c308c6fb1d49c8930d92
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-28 12:33:25 +0200

    Experimenting with Determining the Size of Arrays

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 #include "../utils/greet.hpp"
 
-using namespace std;
-
-void printOddNumbers(int limit) {
-    for (int i = 0; i < limit; ++i) {
-        if (i % 2 != 0) {
-            cout << i << endl;
-        }
-    }
-}
 
 int main() {
 
-    printOddNumbers(10);
+    int numbers[] = {10, 20, 30};
+
+    for (int i = 0; i < std::size(numbers) ; ++i) {
+        std::cout << numbers[i] << std::endl;
+    }
 
     return 0;
 }
