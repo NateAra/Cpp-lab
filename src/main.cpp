78042052093602ce83e5c454b9d323759d8fbb12
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-28 15:18:22 +0200

    Experimenting with bubble sort array

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,35 @@
 #include "../utils/greet.hpp"
 
 /* Exercise
-    * Implement the linear search algorithm
-        * Create a function for finding a value in an int array.
-        * If the target value exists, return its index. Otherwise, return -1.
+    * Implement the bubble sort algorithm
+        * Create a function that takes an array of int and sorts it.
 */
 
-int findValue(int numbers[], int size, int val) {
-    for (int i = 0; i < size; ++i) {
-        if (numbers[i] == val) {
-            return i;
+void swap(int numbers[], int i, int j) {
+    int temp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = temp;
+}
+
+void bubbleSort(int numbers[], int size) {
+    for (int pass = 0; pass < size; ++pass) {
+        for (int i = 1; i < size; ++i) {
+            if (numbers[i] < numbers[i - 1]) {
+                swap(numbers, i, i-1);
+            }
         }
     }
-    return -1;
 }
 
 
 int main() {
 
-    int numbers[] = {1, 3, 5, 11, 8, 12};
-    int value = 2;
+    int numbers[] = {3, 2, 1, 5, 4};
+    bubbleSort(numbers, std::size(numbers));
 
-    std::cout << findValue(numbers, std::size(numbers), value);
-
+    for (int number : numbers) {
+        std::cout << number << std::endl;
+    }
 
     return 0;
 }
