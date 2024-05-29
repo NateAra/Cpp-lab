5ca30ac738bd89b3706c5a7074da9730015a016c
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-29 08:39:09 +0200

    Experimenting with Declaring and Using Pointers

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,17 @@
     *
 */
 
-const int rows = 2;
-const int columns = 3;
-
-void printMatrix(int matrix[rows][columns]) {
-    for (int row = 0; row < rows; row++) {
-        for (int col = 0; col < columns; col++) {
-            std::cout << matrix[row][col] << std::endl;
-        }
-    }
-}
-
+using namespace std;
 
 int main() {
+    int number = 10;
 
-    int matrix[rows][columns] = {
-            {1, 2, 3},
-            {4, 5, 6},
-    };
+    // The address of operator
+    int* ptr = &number;
+    // Indirection (de-referencing) operator
+    *ptr = 20;
 
-    printMatrix(matrix);
+    cout << *ptr;
 
     return 0;
 }
