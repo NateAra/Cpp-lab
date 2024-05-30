fc58ff21bfdb0a366e4426370935a13bc000e0a1
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-30 10:46:54 +0200

    Experimenting with Shared pointers

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,20 @@
 #include <iostream>
+#include "memory"
 #include "../utils/greet.hpp"
 
 using namespace std;
 
 /* Exercise
-    * Create a temp array (Twice the size)
-    * Copy all the elements
-    * Have "numbers" pointer point to the new array
+    *
 */
 
 
 int main() {
-    int initialSize = 5;
-    int* numbers = new int[initialSize];
-    int entries = 0;
+    auto x = make_shared<int>();
+    *x = 10;
+    shared_ptr<int> y(x);
 
-    while (true) {
-        if (entries == initialSize) {
-            // Update the initial size.
-            initialSize *= 2;
-
-            // Create a temp array (Twice the size)
-            int* temp = new int[initialSize];
-
-            copy(numbers, numbers + initialSize, temp);
-            numbers = temp;
-        }
-        cout << "Number: ";
-        cin >> numbers[entries];
-        if (cin.fail()) break;
-        entries++;
-    }
-
-    for (int i = 0; i < entries; ++i) {
-        cout << numbers[i] << endl;
-    }
-
-    delete[] numbers;
+    cout << *y;
 
     return 0;
 }
