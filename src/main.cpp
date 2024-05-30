46021f277242a3f8a744ec7148114f18e52ddc69
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-30 08:37:22 +0200

    Experimenting with Dynamic Memory Allocation

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,21 @@
 using namespace std;
 
 /* Exercise
-    * Given this array:
-        * int numbers[] = {10, 20, 30};
-    * Create a pointer that points to the last element in this array.
-    * using a while loop to iterate over this array, and print the numbers in reverse order.
+    *
 */
 
 int main() {
-    int numbers[] = {10, 20, 30};
-    int* ptr = &numbers[size(numbers) - 1];
+    // Stack
+    int numbers1[10];
 
-    while (ptr >= numbers) {
-        cout << *ptr << endl;
-        ptr--;
-    }
+    // Heap (Free Store)
+    int* numbers = new int[10];
+    delete[] numbers;
+    numbers = nullptr;
+
+    int* number = new int;
+    delete number;
+    number = nullptr;
     
     return 0;
 }
