785b5550c38e24326799f4aa63a307be103710e3
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-31 10:48:45 +0200

    Experimenting with String_Numeric Conversion

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,19 @@
 using namespace std;
 
 /* Exercise
-    * Given the following format for customer number:
-        * 2 alphabetic characters
-        * 4 digits
-    * Write a function to validate a customer number.
+    *
 */
-
-bool isValid(string customerNumber) {
-    if (customerNumber.length() != 6)
-        return false;
-
-    if (!isalpha(customerNumber[0]) && !isalpha(customerNumber[1]))
-        return false;
-
-    for (int i = 2; i < customerNumber.length() ; ++i) {
-        if (isdigit(customerNumber[1]))
-            return false;
-    }
-    return true;
-}
 
 
 int main() {
-    string cNumber = "na2024";
+    string number = "9.99x";
+    double p = 10.40;
 
-    cout << boolalpha << isValid(cNumber);
+    double price = stod(number);
+    string str = to_string(p);
+
+    cout << price << ", " << str;
+
 
     return 0;
 }
