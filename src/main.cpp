fb0c3a5dcd8c6a3d799c36926bc51d7eef6ccb32
NateAra <nate.arafayne@appliedtechnology.se>
2024-05-31 13:20:09 +0200

    Experimenting with Defining Structures Exercise

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,37 @@
 
 using namespace std;
 
-struct Movie {
-    string title;
-    int releaseYear;
+/*Exercise
+    * Define a structure for representing customers with 3 attributes.
+        * ID (Number)
+        * Name
+        * Email
+    * Ask the user for these values, Store them in a Customer Object and print the result.
+*/
+
+struct Customers {
+    int id;
+    string name;
+    string email;
 };
 
 
 int main() {
-    Movie movie;
-    movie.title = "Breaking Bad";
-    movie.releaseYear = 2005;
+    Customers customer;
 
-    cout << movie.title << endl;
-    cout << movie.releaseYear << endl;
+    cout << "Enter customer id: ";
+    cin >> customer.id;
+    cin.ignore();
+
+    cout << "Enter customer name: ";
+    getline(cin, customer.name);
+
+    cout << "Enter customer Email: ";
+    cin >> customer.email;
+
+    cout << "ID: " << customer.id << endl;
+    cout << "Name: " << customer.name << endl;
+    cout << "Email: " << customer.email << endl;
 
 
     return 0;
