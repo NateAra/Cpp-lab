bb94671bc43e6c22a8b69e430ecd23e20fd39824
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-02 17:04:09 +0200

    Experimenting with basic Structures and Functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,36 @@
 
 using namespace std;
 
-/* Exercise
-    * Define a structure for representing a point.
-        * Each point should have "X" and "Y" coordination.
-        * Then overload the equality and stream insertion operators for point.
-*/
-
-struct Point {
-    int x = 0;
-    int y = 0;
+struct ReleaseDate {
+    int year = 0;
+    int month = 0;
 };
 
-bool operator == (const Point& fist, const Point& second) {
-    return (
-            fist.x == second.x &&
-            fist.y == second.y
-    );
+struct Movie {
+    string title;
+    ReleaseDate releaseDate;
+};
+
+ostream& operator << (ostream& stream, const Movie& movie) {
+    stream << "Title: " << movie.title << ", " << "Release Year: " << movie.releaseDate.year;
+    return stream;
 }
 
-ostream& operator << (ostream& stream, const Point& point) {
-    stream << "x: " << point.x << ", " << "Y: " << point.y;
-    return stream;
+Movie getMovie() {
+    Movie movie = {"Terminator", 1984};
+    return movie;
+}
+
+void showMovieTitle(Movie& movie) {
+    cout << movie.title;
 }
 
 
 int main() {
-    Point point1 {5,6};
 
-    Point point2 {5, 6};
+    auto movie = getMovie();
+    showMovieTitle(movie);
 
-    cout << boolalpha << (point1 == point2) << endl;
-
-    cout << "Point1: " << point1 << endl << "Point2: " << point2;
 
     return 0;
 }
