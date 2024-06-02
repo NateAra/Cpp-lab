6179aabfcdfcb79cc60d191b22dbf9a0c8584199
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-02 14:50:57 +0200

    Experimenting with Comparing Structures

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,40 @@
 
 using namespace std;
 
+struct ReleaseDate {
+    int year = 0;
+    int month = 0;
+    int day = 0;
+};
 
-
+struct Movie {
+    string title;
+    ReleaseDate releaseDate;
+};
 
 int main() {
+    Movie movie1 {
+        "Jon Wick",
+        {
+            2019,
+            10,
+            10
+        }
+    };
+
+    Movie movie2 {
+        "Tarzan",
+        {
+            2000,
+            10,
+            1
+        }
+    };
+
+    if (movie1.releaseDate.month == movie2.releaseDate.month) {
+        cout << "Equal";
+    }
+
 
 
     return 0;
