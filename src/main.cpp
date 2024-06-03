eceb88538f6ebf1fb31ceb6f331512eaf793f41a
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-03 12:39:47 +0200

    Experimenting with basics: Converting Values to Strings using stringstream

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include "vector"
-#include "fstream"
+#include "sstream"
 #include "iomanip"
 #include "../utils/greet.hpp"
 
 using namespace std;
 
+string to_string(double number, int precision) {
+    stringstream stream;
+    stream << fixed << setprecision(precision) << number;
+    return stream.str();
+}
+
 int main() {
-    fstream file;
-    file.open("file.txt", ios::in | ios::out | ios::app | ios::binary);
+    double num = 42.34;
+    string str = to_string(num, 2);
 
-    if (file.is_open()) {
-        file.close();
-    }
-
-
+    cout << str;
 
     return 0;
 }
