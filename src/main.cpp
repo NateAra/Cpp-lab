7b71f15c04d4d77f1e50c6d0ff3046593f658e8e
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-03 12:24:11 +0200

    Experimenting with basics: Working with fstreams

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,14 @@
 using namespace std;
 
 int main() {
-    int numbers[3];
-    ifstream file("numbers.dat", ios::binary);
+    fstream file;
+    file.open("file.txt", ios::in | ios::out | ios::app | ios::binary);
 
     if (file.is_open()) {
-        int number;
-        while (file.read(reinterpret_cast<char*>(&number), sizeof(number))){
-            cout << number << endl;
-        }
         file.close();
     }
+
+
 
     return 0;
 }
