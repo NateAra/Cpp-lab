b07a124b9ae80c94ac85da62e70a3cb8367973c9
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-03 13:15:40 +0200

    Experimenting with basics: Parsing Strings using stringstream Exercise

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,33 @@
 
 using namespace std;
 
-int main() {
-    string str = "10 20";
+/* Exercise
+    * Given the following string, write a function to parse this into a Movie structure.
+        * "Terminator 1,1984"
+*/
 
+struct Movie {
+    string title;
+    int year = 0;
+};
+
+Movie parseMovie(const string& str) {
     stringstream stream;
     stream.str(str);
 
-    int first;
-    stream >> first;
+    Movie movie;
+    getline(stream, movie.title, ',');
+    stream >> movie.year;
 
-    int second;
-    stream >> second;
+    return movie;
+}
 
-    cout << first << " + " << second << " = " << first + second;
+int main() {
+    string str = "Terminator 1,1984";
+    Movie movie = parseMovie(str);
+
+    cout << movie.title << endl;
+    cout << movie.year;
 
     return 0;
 }
