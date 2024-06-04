2dbd430d1782da4acd53d504d6a82e5849c199ec
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-04 08:56:45 +0200

    Experimenting with classes: Made a basic rectangel obj

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,17 @@
 #include "sstream"
 #include "iomanip"
 #include "../utils/greet.hpp"
+#include "classes/Rectangle.h"
 
 using namespace std;
 
 
 int main() {
+    Rectangle rectangle{};
+    rectangle.width = 10;
+    rectangle.height = 20;
+
+    cout << rectangle.getArea();
 
     return 0;
 }
