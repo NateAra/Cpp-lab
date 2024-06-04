b78ac688d26baad3b45f828f44f67c3bcd5a3a50
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-04 11:10:01 +0200

    Experimenting with classes: Added Getter & Setter

--- src/classes/Rectangle.cpp
+++ src/classes/Rectangle.cpp
@@ -9,3 +9,23 @@ void Rectangle::draw() {
 int Rectangle::getArea() {
     return width * height;
 }
+
+int Rectangle::getWight() {
+    return width;
+}
+
+void Rectangle::setWight(int width) {
+    if (width < 0)
+        throw std::invalid_argument("Width");
+    this->width = width;
+}
+
+int Rectangle::getHeight() const {
+    return height;
+}
+
+void Rectangle::setHeight(int height) {
+    if (height < 0)
+        throw std::invalid_argument("Height");
+    this->height = height;
+}
