f4676b73c9c33d11a0bda63d541018f68ef3117f
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-04 08:39:16 +0200

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,8 @@
 
 using namespace std;
 
-/* Exercise
-    * Given the following string, write a function to parse this into a Movie structure.
-        * "Terminator 1,1984"
-*/
-
-struct Movie {
-    string title;
-    int year = 0;
-};
-
-Movie parseMovie(const string& str) {
-    stringstream stream;
-    stream.str(str);
-
-    Movie movie;
-    getline(stream, movie.title, ',');
-    stream >> movie.year;
-
-    return movie;
-}
 
 int main() {
-    string str = "Terminator 1,1984";
-    Movie movie = parseMovie(str);
-
-    cout << movie.title << endl;
-    cout << movie.year;
 
     return 0;
 }
