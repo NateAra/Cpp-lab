939ba378ae0ac0ab75dd1ea973fa87b30d3a0b31
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-04 08:43:19 +0200

    Experimenting with classes: Defining a Class

---
+++ src/classes/Rectangle.h
@@ -0,0 +1,13 @@
+#ifndef CPP_LAB_RECTANGLE_H
+#define CPP_LAB_RECTANGLE_H
+
+
+class Rectangle {
+    int width;
+    int height;
+    void draw();
+    int getArea();
+};
+
+
+#endif //CPP_LAB_RECTANGLE_H
