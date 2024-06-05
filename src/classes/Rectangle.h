43a537585706b1293398bca7f31daaa66bf7d553
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-05 10:02:10 +0200

    Experimenting with classes: Constructor Delegation

--- src/classes/Rectangle.h
+++ src/classes/Rectangle.h
@@ -1,19 +1,23 @@
 #ifndef CPP_LAB_RECTANGLE_H
 #define CPP_LAB_RECTANGLE_H
+#include "string"
 
 
 class Rectangle {
 private:
     int width = 0;
     int height = 0;
+    std::string color;
 
 public:
     Rectangle() = default;
     Rectangle(int width, int height);
-    void draw();
-    int getArea();
-    int getWight();
-    void setWight(int width);
+    Rectangle(int width, int height, const std::string &color);
+
+    void draw() const;
+    int getArea() const;
+    int getWidth() const;
+    void setWidth(int width);
     int getHeight() const;
     void setHeight(int height);
 };
