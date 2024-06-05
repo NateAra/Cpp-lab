d5960649c2c2cf1c74d4a188a512cb8697c4342f
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-05 11:17:22 +0200

    Experimenting with classes: Static Members

--- src/classes/Rectangle.h
+++ src/classes/Rectangle.h
@@ -8,19 +8,20 @@ private:
     int width = 0;
     int height = 0;
     std::string color;
+    static int objectCount;
 
 public:
     Rectangle() = default;
     Rectangle(const Rectangle &source);
     Rectangle(int width, int height);
     Rectangle(int width, int height, const std::string &color);
-
     void draw() const;
     int getArea() const;
     int getWidth() const;
     void setWidth(int width);
     int getHeight() const;
     void setHeight(int height);
+    static int getObjectCount();
 };
 
 
