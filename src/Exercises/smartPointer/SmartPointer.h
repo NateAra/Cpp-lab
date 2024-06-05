b0019cb166c272a1d777a5ea0b448edd967ab2c5
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-05 12:25:58 +0200

    Experimenting with classes: Basic smart pointer from scratch

---
+++ src/Exercises/smartPointer/SmartPointer.h
@@ -0,0 +1,16 @@
+#ifndef CPP_LAB_SMARTPOINTER_H
+#define CPP_LAB_SMARTPOINTER_H
+
+
+class SmartPointer {
+private:
+    int* ptr;
+
+public:
+    explicit SmartPointer(int* ptr);
+    ~SmartPointer();
+
+};
+
+
+#endif
