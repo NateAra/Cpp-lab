95ef145a0be9894d8db6866e125bb8a705161898
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-05 12:26:09 +0200

    Experimenting with classes: Basic smart pointer from scratch

---
+++ src/Exercises/smartPointer/SmartPointer.cpp
@@ -0,0 +1,14 @@
+#include "SmartPointer.h"
+#include "iostream"
+
+using namespace std;
+
+SmartPointer::SmartPointer(int *ptr) : ptr{ptr} {
+    cout << "Constructor is called" << endl;
+}
+
+SmartPointer::~SmartPointer() {
+    cout << "Destructor is called" << endl;
+    delete ptr;
+    ptr = nullptr;
+}
