57b186e1147b4c62e965054fe8106cef1d0b4d5a
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-06 10:14:19 +0200

    Experimenting with Operator Overloading: Spaceship Operator

--- src/classes/Length.cpp
+++ src/classes/Length.cpp
@@ -1,7 +1,13 @@
 #include "Length.h"
+#include "compare"
+
 
 Length::Length(int value) : value(value) {}
 
 bool Length::operator==(const Length& other) const {
     return value == other.value;
+}
+
+std::strong_ordering Length::operator<=>(const Length &other) const {
+    return value <=> other.value;
 }
