bbfeb728aa27716c347fc3c20be0501d5a589af7
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-06 11:03:22 +0200

    Experimenting with Operator Overloading: Stream Insertion Operator

--- src/classes/Length.cpp
+++ src/classes/Length.cpp
@@ -3,10 +3,27 @@
 
 Length::Length(int value) : value(value) {}
 
+int Length::getValue() const {
+    return value;
+}
+
+void Length::setValue(int value) {
+    Length::value = value;
+}
+
 bool Length::operator==(const Length& other) const {
     return value == other.value;
 }
 
+bool Length::operator==(int other) const {
+    return value == other;
+}
+
 std::strong_ordering Length::operator<=>(const Length &other) const {
     return value <=> other.value;
+}
+
+
+std::ostream &operator<<(std::ostream &stream, const Length &length) {
+    return stream << length.getValue();
 }
