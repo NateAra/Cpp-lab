ff5c10998493f1b6a127c94c034b7bfb47e652c2
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-06 10:14:35 +0200

    Experimenting with Operator Overloading: Spaceship Operator

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include "classes/Rectangle.h"
+#include "classes/Length.h"
+#include "Exercises/point/Point.h"
 
 
 using namespace std;
 
 
 int main() {
+    Length first{10};
+    Length second{20};
+
+    if (first < second)
+        cout << "Second is greater!";
 
 
     return 0;
