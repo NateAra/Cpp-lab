adbd43e3b9d1f0938429c546a3181cd13eccb462
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-06 11:03:11 +0200

    Experimenting with Operator Overloading: Stream Insertion Operator

--- src/classes/Length.h
+++ src/classes/Length.h
@@ -1,17 +1,24 @@
 #ifndef CPP_LAB_LENGTH_H
 #define CPP_LAB_LENGTH_H
 #include "compare"
+#include "ostream"
 
 class Length {
 private:
     int value;
-
 public:
     explicit Length(int value);
+
+    int getValue() const;
+    void setValue(int value);
+
     bool operator==(const Length& other) const;
+    bool operator==(int other) const;
     std::strong_ordering operator<=>(const Length& other) const;
 
 };
+
+std::ostream& operator<<(std::ostream& stream, const Length& length);
 
 
 #endif
