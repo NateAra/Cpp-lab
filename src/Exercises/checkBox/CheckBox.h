b05a44abb0dc9b9b1f16fff5047b48a7eee229e7
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-07 11:06:45 +0200

    Experimenting with: Polymorphism

---
+++ src/Exercises/checkBox/CheckBox.h
@@ -0,0 +1,11 @@
+#ifndef CPP_LAB_CHECKBOX_H
+#define CPP_LAB_CHECKBOX_H
+#include "../textBox/TextBox.h"
+
+
+class CheckBox : public TextBox {
+public:
+    void draw() const override;
+};
+
+#endif
