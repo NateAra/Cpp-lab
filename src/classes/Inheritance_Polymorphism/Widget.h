5f1c350d671792e336afbf410a7298160938ecd5
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-07 07:20:05 +0200

    Experimenting with: Basic Inheritance

---
+++ src/classes/Inheritance_Polymorphism/Widget.h
@@ -0,0 +1,14 @@
+#ifndef CPP_LAB_WIDGET_H
+#define CPP_LAB_WIDGET_H
+
+class Widget {
+private:
+    bool enabled;
+public:
+    void enable();
+    void disable();
+    bool isEnabled() const;
+
+};
+
+#endif
