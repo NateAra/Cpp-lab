2d916cdd162256779ee0f66c05a42230047955ba
NateAra <nate.arafayne@appliedtechnology.se>
2024-06-09 22:15:03 +0200

    Experimenting With Rethrowing an Exception: Created function for testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,15 @@
 #include "vector"
 #include "classes/Rectangle.h"
 
+void createRectangle() {
+    Rectangle react;
+    react.setWidth(-1);
+}
+
 using namespace std;
 
 int main() {
-    try {
-        cout << "Width: ";
-        int width;
-        cin >> width;
 
-        Rectangle rect;
-        rect.setWidth(width);
-    } catch (const logic_error& ex) {
-        cout << ex.what();
-    }
 
     return 0;
 }
